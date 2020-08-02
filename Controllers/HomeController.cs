e27554133ed87f2fb16387d8b8da1cd6ee77e968
raihanM95 <mahmud.raihan@yahoo.com>
2020-08-03 02:12:58 +0600

    contact

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace CityProblemSolver.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
