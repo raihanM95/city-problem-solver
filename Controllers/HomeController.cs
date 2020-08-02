6554d6fd4ad7e11e79a8eadf53d238520fc67a85
raihanM95 <mahmud.raihan@yahoo.com>
2020-08-03 01:35:10 +0600

    about

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace CityProblemSolver.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
