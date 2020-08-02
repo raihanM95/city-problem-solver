0fa64ae0bfdf36968c3cda8cec2fcbcb47f23b90
raihanM95 <mahmud.raihan@yahoo.com>
2020-08-03 01:49:55 +0600

    services

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace CityProblemSolver.Controllers
             return View();
         }
 
+        public IActionResult Services()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
