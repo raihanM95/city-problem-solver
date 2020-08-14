4e85e4002a3112b2d714c54323b83ea336710b82
raihanM95 <mahmud.raihan@yahoo.com>
2020-08-15 02:06:42 +0600

    user-worker-layout

---
+++ Controllers/WorkerController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityProblemSolver.Controllers
+{
+    public class WorkerController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
